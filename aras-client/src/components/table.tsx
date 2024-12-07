394a56100b35d39d44d43ea3bf9060619e3a8a9a
Dmitrii Shulaikin <dmshulaikin@gmail.com>
2024-12-07 14:13:11 +0100

    added horizontal scrolling in case table has a lot of columns

--- aras-client/src/components/table.tsx
+++ aras-client/src/components/table.tsx
@@ -1,95 +1,116 @@
 import { useMemo } from "react";
-import { Box, IconButton, Table } from "@mui/joy";
+import { Box, Divider, IconButton, Table } from "@mui/joy";
 import { flexRender, Table as TableType } from "@tanstack/react-table";
-import { FaAngleLeft, FaAngleRight } from 'react-icons/fa';
+import { FaAngleLeft, FaAngleRight } from "react-icons/fa";
 import { getRiskClass } from "../utils";
 import { ArasSelect } from "./select";
 
 export interface Record {
-    risk?: number;
+  risk?: number;
 }
 
 interface TableProps<T extends Record> {
-    table: TableType<T>;
+  table: TableType<T>;
 }
 
 interface PaginationProps {
-    pageOptions: number[];
-    onPageSizeChange: (pageSize: number) => void;
-    onNextPage: () => void;
-    onPrevPage: () => void;
-    hasNext: boolean;
-    hasPrev: boolean;
+  pageOptions: number[];
+  onPageSizeChange: (pageSize: number) => void;
+  onNextPage: () => void;
+  onPrevPage: () => void;
+  hasNext: boolean;
+  hasPrev: boolean;
 }
 
-export const Pagination = ({ 
-    onPageSizeChange, 
-    onNextPage, 
-    onPrevPage,
-    hasNext,
-    hasPrev,
-    pageOptions,
+export const Pagination = ({
+  onPageSizeChange,
+  onNextPage,
+  onPrevPage,
+  hasNext,
+  hasPrev,
+  pageOptions,
 }: PaginationProps) => {
-    const options = useMemo(() => pageOptions.map((x) => x.toString()), [pageOptions]);
+  const options = useMemo(
+    () => pageOptions.map((x) => x.toString()),
+    [pageOptions]
+  );
 
-    return <Box display='flex' gap={1} justifyContent='space-between' width='100%'>
-        <Box display='flex' alignContent='center' gap={1} sx={{ verticalAlign: 'middle' }}>
-            <ArasSelect size='sm' label='Page size' options={options} onChange={(_e, v) => onPageSizeChange((v && parseInt(v)) || 5)}/> 
-        </Box>
-        <Box>
-            <IconButton disabled={!hasPrev} onClick={onPrevPage}><FaAngleLeft /></IconButton>
-            <IconButton disabled={!hasNext} onClick={onNextPage}><FaAngleRight /></IconButton>
-       </Box>
+  return (
+    <Box display="flex" gap={1} justifyContent="space-between">
+      <Box
+        display="flex"
+        alignContent="center"
+        gap={1}
+        sx={{ verticalAlign: "middle" }}
+      >
+        <ArasSelect
+          size="sm"
+          label="Page size"
+          options={options}
+          onChange={(_e, v) => onPageSizeChange((v && parseInt(v)) || 5)}
+        />
+      </Box>
+      <Box>
+        <IconButton disabled={!hasPrev} onClick={onPrevPage}>
+          <FaAngleLeft />
+        </IconButton>
+        <IconButton disabled={!hasNext} onClick={onNextPage}>
+          <FaAngleRight />
+        </IconButton>
+      </Box>
     </Box>
-}
+  );
+};
 
 const PAGE_SIZES = [10, 20, 30, 40, 50];
 
-export const ArasTable = <T extends Record,>({table}: TableProps<T>) => {
-    return <Table
-        borderAxis="xBetween"
-        color="neutral"
-    >
-        <thead>
-        {table.getHeaderGroups().map(headerGroup => (
-            <tr key={headerGroup.id}>
-            {headerGroup.headers.map(header => (
-                <th key={header.id}>
-                {header.isPlaceholder
-                    ? null
-                    : flexRender(
-                        header.column.columnDef.header,
-                        header.getContext()
-                    )}
-                </th>
+export const ArasTable = <T extends Record>({ table }: TableProps<T>) => {
+  return (
+    <>
+      <Box overflow="scroll">
+        <Table borderAxis="xBetween" color="neutral">
+          <thead style={{ overflow: "scroll" }}>
+            {table.getHeaderGroups().map((headerGroup) => (
+              <tr key={headerGroup.id}>
+                {headerGroup.headers.map((header) => (
+                  <th key={header.id} className="cell" {...{ width: 200 }}>
+                    {header.isPlaceholder
+                      ? null
+                      : flexRender(
+                          header.column.columnDef.header,
+                          header.getContext()
+                        )}
+                  </th>
+                ))}
+              </tr>
             ))}
-            </tr>
-        ))}
-        </thead>
-        <tbody>
-        {table.getRowModel().rows.map(row => (
-            <tr key={row.id} className={getRiskClass(row.original.risk)}>
-            {row.getVisibleCells().map(cell => (
-                <td key={cell.id}>
-                {flexRender(cell.column.columnDef.cell, cell.getContext())}
-                </td>
+          </thead>
+          <tbody>
+            {table.getRowModel().rows.map((row) => (
+              <tr key={row.id} className={getRiskClass(row.original.risk)}>
+                {row.getVisibleCells().map((cell) => (
+                  <td key={cell.id} className="cell">
+                    {flexRender(cell.column.columnDef.cell, cell.getContext())}
+                  </td>
+                ))}
+              </tr>
             ))}
-            </tr>
-        ))}
-        </tbody>
-        <tfoot style={{ width: '100%' }}>
-            <tr>
-                <td colSpan={table.getAllColumns().length}>
-                    <Pagination 
-                        pageOptions={PAGE_SIZES}
-                        onPageSizeChange={(pageSize: number) => { table.setPageSize(pageSize) }}
-                        hasNext={table.getCanNextPage()}
-                        hasPrev={table.getCanPreviousPage()}
-                        onNextPage={() => table.nextPage()}
-                        onPrevPage={() => table.previousPage()}
-                    />
-                </td>
-            </tr>
-        </tfoot>
-    </Table>
-}
+          </tbody>
+        </Table>
+      </Box>
+      <Divider sx={{ pt: 0 }} />
+      <Box py={1} px={2}>
+        <Pagination
+          pageOptions={PAGE_SIZES}
+          onPageSizeChange={(pageSize: number) => {
+            table.setPageSize(pageSize);
+          }}
+          hasNext={table.getCanNextPage()}
+          hasPrev={table.getCanPreviousPage()}
+          onNextPage={() => table.nextPage()}
+          onPrevPage={() => table.previousPage()}
+        />
+      </Box>
+    </>
+  );
+};
