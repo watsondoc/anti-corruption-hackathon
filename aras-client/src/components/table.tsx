a778e278be84559e5d35ca04b282adac1e5b86f0
Dmitrii Shulaikin <dmshulaikin@gmail.com>
2024-12-07 13:27:23 +0100

    added pagination to the table

--- aras-client/src/components/table.tsx
+++ aras-client/src/components/table.tsx
@@ -1,6 +1,9 @@
-import { Table } from "@mui/joy";
+import { useMemo } from "react";
+import { Box, IconButton, Table } from "@mui/joy";
 import { flexRender, Table as TableType } from "@tanstack/react-table";
+import { FaAngleLeft, FaAngleRight } from 'react-icons/fa';
 import { getRiskClass } from "../utils";
+import { ArasSelect } from "./select";
 
 export interface Record {
     risk?: number;
@@ -9,6 +12,38 @@ export interface Record {
 interface TableProps<T extends Record> {
     table: TableType<T>;
 }
+
+interface PaginationProps {
+    pageOptions: number[];
+    onPageSizeChange: (pageSize: number) => void;
+    onNextPage: () => void;
+    onPrevPage: () => void;
+    hasNext: boolean;
+    hasPrev: boolean;
+}
+
+export const Pagination = ({ 
+    onPageSizeChange, 
+    onNextPage, 
+    onPrevPage,
+    hasNext,
+    hasPrev,
+    pageOptions,
+}: PaginationProps) => {
+    const options = useMemo(() => pageOptions.map((x) => x.toString()), [pageOptions]);
+
+    return <Box display='flex' gap={1} justifyContent='space-between' width='100%'>
+        <Box display='flex' alignContent='center' gap={1} sx={{ verticalAlign: 'middle' }}>
+            <ArasSelect size='sm' label='Page size' options={options} onChange={(_e, v) => onPageSizeChange((v && parseInt(v)) || 5)}/> 
+        </Box>
+        <Box>
+            <IconButton disabled={!hasPrev} onClick={onPrevPage}><FaAngleLeft /></IconButton>
+            <IconButton disabled={!hasNext} onClick={onNextPage}><FaAngleRight /></IconButton>
+       </Box>
+    </Box>
+}
+
+const PAGE_SIZES = [10, 20, 30, 40, 50];
 
 export const ArasTable = <T extends Record,>({table}: TableProps<T>) => {
     return <Table
@@ -42,5 +77,19 @@ export const ArasTable = <T extends Record,>({table}: TableProps<T>) => {
             </tr>
         ))}
         </tbody>
+        <tfoot style={{ width: '100%' }}>
+            <tr>
+                <td colSpan={table.getAllColumns().length}>
+                    <Pagination 
+                        pageOptions={PAGE_SIZES}
+                        onPageSizeChange={(pageSize: number) => { table.setPageSize(pageSize) }}
+                        hasNext={table.getCanNextPage()}
+                        hasPrev={table.getCanPreviousPage()}
+                        onNextPage={() => table.nextPage()}
+                        onPrevPage={() => table.previousPage()}
+                    />
+                </td>
+            </tr>
+        </tfoot>
     </Table>
 }
--- aras-client/src/pages/declarations.tsx
+++ aras-client/src/pages/declarations.tsx
@@ -2,9 +2,10 @@ import { Link as RouterLink } from "react-router";
 import {
   createColumnHelper,
   getCoreRowModel,
+  getPaginationRowModel,
   useReactTable,
 } from "@tanstack/react-table";
-import { Box, Card, CardContent, Input, Link, List, ListItem, Sheet, Typography } from "@mui/joy";
+import { Box, Button, Card, CardContent, Input, Link, List, ListItem, Typography } from "@mui/joy";
 import { Layout } from "../components/layout";
 import { ArasTable } from "../components/table";
 import { ArasSelect } from "../components/select";
@@ -68,6 +69,54 @@ const data: Declaration[] = [
     risk: 0.01,
     income: 95000,
   },
+  {
+    id: "11",
+    name: "John Doe",
+    position: "Mayor",
+    declarationDate: "2023",
+    risk: 1,
+    income: 98000,
+  },
+  {
+    id: "12",
+    name: "Jane Smith",
+    position: "Council Member",
+    declarationDate: "2024",
+    risk: 0.78,
+    income: 88000,
+  },
+  {
+    id: "13",
+    name: "Alice Johnson",
+    position: "Treasurer",
+    declarationDate: "2023",
+    risk: 0.56,
+    income: 93000,
+  },
+  {
+    id: "14",
+    name: "Bob Brown",
+    position: "Secretary",
+    declarationDate: "2024",
+    risk: 0.32,
+    income: 83000,
+  },
+  {
+    id: "15",
+    name: "Charlie Davis",
+    position: "Chief of Staff",
+    declarationDate: "2023",
+    risk: 0.25,
+    income: 91000,
+  },
+  {
+    id: "16",
+    name: "David Wilson",
+    position: "Deputy Mayor",
+    declarationDate: "2023",
+    risk: 0.01,
+    income: 95000,
+  },
 ];
 
 const columnHelper = createColumnHelper<Declaration>();
@@ -120,6 +169,12 @@ export const DeclarationsPage = () => {
     columns,
     data,
     getCoreRowModel: getCoreRowModel(),
+    getPaginationRowModel: getPaginationRowModel(),
+    initialState: {
+      pagination: {
+        pageSize: 10,
+      }
+    }
   });
 
   return (
