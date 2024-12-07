79305def8fdc57b89e88348e5d5c570b0d165d84
Artem <artem.shipitsyn@intapp.com>
2024-12-07 17:13:52 +0400

    added draft for parser

--- src/app.ts
+++ src/app.ts
@@ -2,6 +2,8 @@ import express from "express";
 import dotenv from "dotenv";
 import exampleRoutes from "./routes/webRoutes";
 import { connectToDatabase } from "./db/mongoClient";
+import { JSONToMongoParser } from './parser/JSONToMongoParser';
+import * as path from 'path';
 
 dotenv.config();
 
@@ -15,6 +17,11 @@ app.use("/api", exampleRoutes);
 // Start the server and connect to MongoDB
 const startServer = async () => {
   await connectToDatabase();
+
+  const parser = new JSONToMongoParser('corruption', 'declarations');
+
+  await parser.parseAndInsert(path.resolve(__dirname, '../seedData/details_2024.json'));
+
   app.listen(port, () => {
     console.log(`Server running on http://localhost:${port}`);
   });
